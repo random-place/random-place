c6c2e16f1c0ad5c1cfd1133c67f066ffcb12e861
BeomsooKim <kbsat@naver.com>
2020-11-26 15:21:03 +0900

    item의 세분화

--- rand_place_app/views.py
+++ rand_place_app/views.py
@@ -28,18 +28,25 @@ def random(request):
     # print("현재 장소 :", pname, areaCode, sigunguCode)
     if sigunguCode == 0:
         sigunguCode = ""
-    payload = {'ServiceKey': api_key_decode, 'numOfRows': 12, 'pageNo': 1,
-               'MobileOS': 'ETC', 'MobileApp': 'AppTest', 'areaCode': areaCode, 'sigunguCode': sigunguCode, 'cat1': 'A02', 'listYN': 'Y', '_type': 'json', 'arrange': 'P', 'overviewYN': 'Y'}
+    contentIdList = [12, 14, 15, 28]
+    items = []
+    # contentTypeId : 12 (관광) , 14( 문화) , 15 (공연,축제), 28 (레포츠),
+    for i, contentId in enumerate(contentIdList):
+        payload = {'ServiceKey': api_key_decode, 'numOfRows': 9, 'pageNo': 1, 'contentTypeId': contentId,
+                   'MobileOS': 'ETC', 'MobileApp': 'AppTest', 'areaCode': areaCode, 'sigunguCode': sigunguCode, 'listYN': 'Y', '_type': 'json', 'arrange': 'P', 'overviewYN': 'Y'}
+        r = requests.get(
+            'http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList', params=payload)
+        data = r.json()
+        items.append(data['response']['body']['items']['item'])
 
-    r = requests.get(
-        'http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList', params=payload)
+    # payload = {'ServiceKey': api_key_decode, 'numOfRows': 10, 'pageNo': 1, 'contentTypeId': 28,
+    #            'MobileOS': 'ETC', 'MobileApp': 'AppTest', 'areaCode': areaCode, 'sigunguCode': sigunguCode, 'listYN': 'Y', '_type': 'json', 'arrange': 'P', 'overviewYN': 'Y'}
 
-    data = r.json()
-    # print(data)
-    items = data['response']['body']['items']['item']
-    print(items[1])
-    for item in items:
-        print(item['title'])
-        print(item)
+    # data = r.json()
+    # items = data['response']['body']['items']['item']
+    # print(items[1])
+    # for item in items:
+    #     print(item['title'])
+    #     print(item)
 
-    return render(request, 'random.html', {'items': items, 'place': place})
+    return render(request, 'random.html', {'item1': items[0], 'item2': items[1], 'item3': items[2], 'item4': items[3], 'place': place})
